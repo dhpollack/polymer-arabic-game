b5e4f16b001477d4518c578f10c14939881f08dd
David Pollack <sent@da3.net>
2015-04-02 09:32:03 +0000

    missing diacritics util

---
+++ utils/diacritics.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# coding=UTF-8
+import unicodedata
+import json
+import codecs
+import sys
+
+filetoread = sys.argv
+
+def strip_accents(s):
+   return ''.join(c for c in unicodedata.normalize('NFD', s)
+                  if unicodedata.category(c) != 'Mn')
+newjson = []
+
+with codecs.open(filetoread[1],'r', 'utf-8') as data_file:    
+    data = json.load(data_file)
+    for word in data:
+        entry = [ word[0], strip_accents(word[0]), word[1] ]
+        newjson.append(entry)
+    with codecs.open(filetoread[1].replace(".json", "-stpacc.json"), 'w', 'utf-8') as writejson:
+        json.dump(newjson, writejson, ensure_ascii=False)
+   
+    
