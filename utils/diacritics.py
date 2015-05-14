ae0f374820746e26ff3fe72a5e46c0f752010388
David Pollack <sent@da3.net>
2015-05-14 18:32:12 +0000

    changed json format

--- utils/diacritics.py
+++ utils/diacritics.py
@@ -15,14 +15,8 @@ newjson = []
 with codecs.open(filetoread[1],'r', 'utf-8') as data_file:    
     data = json.load(data_file)
     for word in data:
-        entry = []
-        for val in word:
-            i = word.index(val)
-            if i == 0:
-                entry.append(val)
-                entry.append(strip_accents(val))
-            else:
-                entry.append(val)
+        entry = word
+        entry['ohne'] = strip_accents(word['word'])
         newjson.append(entry)
     with codecs.open(filetoread[1].replace(".json", "-stpacc.json"), 'w', 'utf-8') as writejson:
         json.dump(newjson, writejson, ensure_ascii=False)
