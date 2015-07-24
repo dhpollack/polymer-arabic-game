b1a3a96cc94de17f990fbf107b6b73dfeff4b199
David Pollack <sent@da3.net>
2015-07-24 12:20:45 +0000

    initial routing commit. doesn't really do anything yet

---
+++ app/assets/js/app.js
@@ -0,0 +1,35 @@
+(function(document) {
+  'use strict';
+
+  // Grab a reference to our auto-binding template
+  // and give it some initial binding values
+  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+
+  // Listen for template bound event to know when bindings
+  // have resolved and content has been stamped to the page
+  //app.addEventListener('dom-change', function() {
+    //console.log('Our app is ready to rock!');
+  //});
+
+  // See https://github.com/Polymer/polymer/issues/1381
+  window.addEventListener('WebComponentsReady', function() {
+    // imports are loaded and elements have been registered
+    var app = document.querySelector('tinavg-webapp');
+  });
+
+  window.cookieconsent_options = {
+      learnMore: 'more info',
+      link: 'https://www.tinavg.com/terms.html',
+      theme: 'dark-top'
+  };
+  setTimeout(function() {
+    if(document.querySelector('[unresolved]')) {
+      document.getElementById('friendly_message').removeAttribute('hidden');
+      document.querySelector('[unresolved]').removeAttribute("unresolved");
+    }
+  }, 25000);
+  function hideFriendlyMessage() {
+    document.getElementById('friendly_message').setAttribute('hidden', true);
+  }
+
+})(document);
